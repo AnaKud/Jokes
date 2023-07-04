11a2dd9171bab1b823deb725b384be0fe0d88f7c
ANAKUD <MIKAHARY@YANDEX.RU>
2023-07-04 20:47:43 +0300

    Изменения на семинаре

--- Jokes/ViewController.swift
+++ Jokes/ViewController.swift
@@ -7,6 +7,12 @@ class ViewController: UIViewController {
 	@IBOutlet weak var jokeView: UIView!
 	@IBOutlet weak var setupJokeLabel: UILabel!
 
+	private let jokes = JokeModelsMock()
+	private var joke: JokeModel {
+		jokes[currentIndex]
+	}
+	var currentIndex = 0
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 	}
@@ -14,6 +20,11 @@ class ViewController: UIViewController {
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		roundCornerForAllViews()
+		showJoke()
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		showAlert(title: "Привет", message: "Юзер")
 	}
 
 	func roundCornerForAllViews() {
@@ -29,12 +40,26 @@ class ViewController: UIViewController {
 	func setupData() {
 	}
 
-	func showJoke(_ joke: JokeModel) {
+	func showJoke() {
+		setupJokeLabel.text = joke.setup
 	}
 
 	@IBAction func showPunchButtonTapped(_ sender: Any) {
+		showAlert(title: "Punchline", message: joke.punchline)
 	}
 
 	@IBAction func refreshButtonTapped(_ sender: Any) {
+		if currentIndex == jokes.jokesCount - 1 {
+			currentIndex = -1
+		}
+		currentIndex += 1
+		showJoke()
+	}
+
+	func showAlert(title: String, message: String) {
+		let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: "Ok", style: .default)
+		alert.addAction(action)
+		present(alert, animated: true)
 	}
 }
